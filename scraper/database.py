e64dba26b0d20696f8524e64044247c857496b83
chribsen <chribsen@yahoo.dk>
2016-09-11 22:38:34 +0200

    changed path to absolute path in parent dir

--- scraper/database.py
+++ scraper/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/athletes.db', convert_unicode=True)
+engine = create_engine('sqlite:////Users/Christian/Documents/Workspaces/python_scripts/leapcraft-ironman/scraper/athletes-v2.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
