504382f9c8a74cc9afb9280250267164d4a35a35
chribsen <chribsen@yahoo.dk>
2016-09-11 22:37:18 +0200

    added sorting to api + removed index.html rendering

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify, request
+from sqlalchemy import asc, desc
 from scraper.database import db_session
 from scraper.models import Athlete
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
+
+@app.route('/')
+def index():
+    return app.send_static_file('templates/index.html')
 
 @app.route('/athletes')
 def athletes():
@@ -15,8 +20,10 @@ def athletes():
     if exclude:
         exclude = exclude.split(',')
 
-    athletes = [athlete.as_dict(exclude=exclude) for athlete in Athlete.query.all()]
+    athletes = [athlete.as_dict(exclude=exclude) for athlete in Athlete.query.order_by(asc(Athlete.overall_rank)).all()]
     return jsonify(dict(athletes=athletes))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
